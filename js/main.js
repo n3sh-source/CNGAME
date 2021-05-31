84f26e43041adea25238c56dc7ac71f9e03dd02b
n3sh-source <gustavo.carakaki@gmail.com>
2021-05-31 17:08:31 -0300

    iniciando o web scraping da secundaria

--- js/main.js
+++ js/main.js
@@ -1,2 +1,38 @@
 //Feito por: Gustavo Costa Arakaki - TP-P2-PSI - FATEC-RL
 //Feito por: Luca Pedro de Oliveira - TP-P2-PSI - FATEC-RL
+
+//Section Secundária
+function NSPegarDados(){
+	fetch("https://cors-anywhere.herokuapp.com/https://g1.globo.com/pop-arte/games/")
+		.then(resp => resp.text())
+		.then(ns => {
+			let dom = new DOMParser();
+			let documento = dom.parseFromString(ns,"text/html");
+			NSraspar(documento);
+		})
+		.catch(e => document.write(e));
+};
+
+function NSraspar(documento){
+    var ref = document.getElementById('flex_secundaria');
+
+    var noticia = document.createElement('div');
+    noticia.setAttribute('class', 'box_secundaria');
+
+    var div_img = document.createElement('div');
+    div_img.setAttribute('class', 'img_secundaria');
+
+    var img = document.createElement('img');
+    img.setAttribute('class', 'img_div_sec');
+
+    //feed-post-body feed-post-link
+	documento.querySelectorAll(".feed-post-body").forEach(a => {
+		noticia.appendChild(a);  
+	});
+
+    
+    ref.appendChild(noticia);
+};
+
+
+window.onload = NSPegarDados;
