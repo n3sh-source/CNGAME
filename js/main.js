cec4e182899e0580da55478baa850c8039369dbf
n3sh-source <gustavo.carakaki@gmail.com>
2021-06-01 16:53:01 -0300

    estrutura da noticia secundaria

--- js/main.js
+++ js/main.js
@@ -16,18 +16,54 @@ function NSPegarDados(){
 function NSraspar(documento){
     var ref = document.getElementById('flex_secundaria');
 
-    var div_img = document.createElement('div');
-    div_img.setAttribute('class', 'img_secundaria');
-
-    var img = document.createElement('img');
-    img.setAttribute('class', 'img_div_sec');
-
     //feed-post-body feed-post-link
 	documento.querySelectorAll(".feed-post-body").forEach(div => {
+		//Esqueleto da notícia
         var noticia = document.createElement('div');
         noticia.setAttribute('class', 'box_secundaria');
         
+			//Secundaria img
+		var div_img = document.createElement('div');
+    	div_img.setAttribute('class', 'img_secundaria');
 
+    	var img = document.createElement('img');
+    	img.setAttribute('class', 'img_div_sec');
+
+			//Secundaria info
+		var div_info_secundaria = document.createElement('div');
+		div_info_secundaria.setAttribute('class', 'info_secundaria');
+				//Secundaria texto
+		var texto_secundaria = document.createElement('div');
+		texto_secundaria.setAttribute('class', 'texto_secundaria');
+
+		var p_texto_secundaria = document.createElement('p');
+
+				//Secundaria categoria e data
+		var cat_data = document.createElement('div');
+		cat_data.setAttribute('class', 'cat_data');
+
+		var cat = document.createElement('div');
+		cat.setAttribute('class', 'cat');
+
+		var p_cat = document.createElement('p');
+				//Data
+		var data = document.createElement('div');
+		data.setAttribute('class', 'data');
+
+		var p_data = document.createElement('p');
+
+		data.appendChild(p_data);
+		cat.appendChild(p_cat);
+		cat_data.appendChild(cat);
+		cat_data.appendChild(data);
+		texto_secundaria.appendChild(p_texto_secundaria);
+		div_info_secundaria.appendChild(texto_secundaria);
+		div_info_secundaria.appendChild(cat_data);
+		div_img.appendChild(img);
+
+		noticia.appendChild(div_img);
+		noticia.appendChild(div_info_secundaria);
+		
 		noticia.appendChild(div);
 
         ref.appendChild(noticia);
